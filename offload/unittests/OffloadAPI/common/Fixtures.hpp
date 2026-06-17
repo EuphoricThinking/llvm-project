189ff3e662bab7ecd6e5854aa3450cc93c61afff
Agata Momot <agata.momot@intel.com>
2026-06-17 05:41:42 -0700

    at least COMPILES, But I need to test get device and host

--- offload/unittests/OffloadAPI/common/Fixtures.hpp
+++ offload/unittests/OffloadAPI/common/Fixtures.hpp
@@ -381,17 +381,17 @@ struct LaunchSingleKernelTestBase : LaunchKernelTestBase {
   ol_symbol_handle_t Kernel = nullptr;
 };
 
-// using DevicesVec = std::vector<TestEnvironment::Device>;
+using DevicesVec = std::vector<TestEnvironment::Device>;
 
-// DevicesVec getDevicesAndHost() {
-//   DevicesVec Res(TestEnvironment::getDevices());
+inline DevicesVec getDevicesAndHost() {
+  DevicesVec Res(TestEnvironment::getDevices());
 
-//   TestEnvironment::Device Host{TestEnvironment::getHostDevice(), "HOST"};
+  TestEnvironment::Device Host{TestEnvironment::getHostDevice(), "HOST"};
 
-//   Res.push_back(Host);
+  Res.push_back(Host);
 
-//   return Res;
-// }
+  return Res;
+}
 
 template <class T>
 inline std::string defaultPrinterWithParam(const ::testing::TestParamInfo<
