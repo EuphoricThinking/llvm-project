6f1a50a6aa9afccfa17de5ec42d428f79328e98b
Agata Momot <agata.momot@intel.com>
2026-06-25 23:47:16 -0700

    fixed? getmmem info test needs a real pointer

--- offload/unittests/OffloadAPI/memory/olGetMemInfoSize.cpp
+++ offload/unittests/OffloadAPI/memory/olGetMemInfoSize.cpp
@@ -31,7 +31,19 @@ struct olGetMemInfoSizeTypesTest : olPropertyTest<ol_mem_info_t> {
 OFFLOAD_TESTS_INSTANTIATE_DEVICE_FIXTURE_WITH_PARAM(olGetMemInfoSizeTypesTest, testing::ValuesIn(MemInfoSizeProperties), defaultPropertyTestPrinter<ol_mem_info_t>);
 
 struct olGetMemInfoSizeTest : OffloadDeviceTest {
-  void* DummyPtr = reinterpret_cast<void*>(0x1234);
+  void *OffsetPtr() { return &reinterpret_cast<char *>(Ptr)[123]; }
+
+  void SetUp() override {
+    RETURN_ON_FATAL_FAILURE(OffloadDeviceTest::SetUp());
+    ASSERT_SUCCESS(olMemAlloc(Device, OL_ALLOC_TYPE_DEVICE, 0x1024, &Ptr));
+  }
+
+  void TearDown() override {
+    ASSERT_SUCCESS(olMemFree(Ptr));
+    RETURN_ON_FATAL_FAILURE(OffloadDeviceTest::TearDown());
+  }
+
+  void *Ptr;
 };
 
 OFFLOAD_TESTS_INSTANTIATE_DEVICE_FIXTURE(olGetMemInfoSizeTest);
@@ -63,10 +75,10 @@ TEST_P(olGetMemInfoSizeTypesTest, Success) {
 TEST_P(olGetMemInfoSizeTest, InvalidSymbolInfoEnumeration) {
   size_t Size = 0;
   ASSERT_ERROR(OL_ERRC_INVALID_ENUMERATION,
-               olGetMemInfoSize(DummyPtr, OL_MEM_INFO_FORCE_UINT32, &Size));
+               olGetMemInfoSize(Ptr, OL_MEM_INFO_FORCE_UINT32, &Size));
 }
 
 TEST_P(olGetMemInfoSizeTest, InvalidNullPointer) {
   ASSERT_ERROR(OL_ERRC_INVALID_NULL_POINTER,
-               olGetMemInfoSize(DummyPtr, OL_MEM_INFO_DEVICE, nullptr));
+               olGetMemInfoSize(Ptr, OL_MEM_INFO_DEVICE, nullptr));
 }
